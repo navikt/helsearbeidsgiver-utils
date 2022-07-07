6627dda4fc4340b27bce288b577c5cb65f22486f
Mikael Bjerga <mikael.bjerga@nav.no>
2022-07-07 16:27:56 +0200

    Legg til funksjon for å lage logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.helsearbeidsgiver"
-version = "0.1.0"
+version = "0.1.1"
 
 val githubPassword: String by project
 
---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/LogUtils.kt
@@ -0,0 +1,7 @@
+package no.nav.helsearbeidsgiver.utils
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+fun <T : Any> T.logger(): Logger =
+    LoggerFactory.getLogger(this.javaClass)

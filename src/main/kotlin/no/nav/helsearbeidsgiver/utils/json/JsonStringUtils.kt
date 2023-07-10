d66e306ae26afc67ee8c1756332ed18475d2c750
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-10 10:12:06 +0200

    Legg til metode for å lage lesbar json

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/json/JsonStringUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/json/JsonStringUtils.kt
@@ -1,6 +1,14 @@
 package no.nav.helsearbeidsgiver.utils.json
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+
+private val jsonPrettyPrint = Json { prettyPrint = true }
+
 private val jsonWhitespaceRegex = Regex("""("(?:\\"|[^"])*")|\s""")
+
+fun JsonElement.toPretty(): String =
+    jsonPrettyPrint.encodeToString(JsonElement.serializer(), this)
 
 /** NB! I hovedsak brukt i tester. Bruk i prod med omhu. */
 fun String.removeJsonWhitespace(): String =

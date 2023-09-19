8810953aa8b1bd7d76519db3cf54ebd61b919a48
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-09-19 15:02:58 +0200

    Legg til serialiserer for OffsetDateTime (#14)

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/json/serializer/DateSerializers.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/json/serializer/DateSerializers.kt
@@ -2,6 +2,7 @@ package no.nav.helsearbeidsgiver.utils.json.serializer
 
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import java.time.YearMonth
 
 object YearMonthSerializer : AsStringSerializer<YearMonth>(
@@ -17,4 +18,9 @@ object LocalDateSerializer : AsStringSerializer<LocalDate>(
 object LocalDateTimeSerializer : AsStringSerializer<LocalDateTime>(
     serialName = "helsearbeidsgiver.kotlinx.LocalDateTimeSerializer",
     parse = LocalDateTime::parse
+)
+
+object OffsetDateTimeSerializer : AsStringSerializer<OffsetDateTime>(
+    serialName = "helsearbeidsgiver.kotlinx.OffsetDateTimeSerializer",
+    parse = OffsetDateTime::parse
 )

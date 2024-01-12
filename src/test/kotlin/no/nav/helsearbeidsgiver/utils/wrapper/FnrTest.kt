223a451dd2289a9ea0b8c9ec991a6d3c014d6db8
Mikael Bjerga <mikael.bjerga@nav.no>
2024-01-12 13:17:43 +0100

    Ta høyde for kontrollsiffer i tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/utils/wrapper/FnrTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/utils/wrapper/FnrTest.kt
@@ -11,25 +11,24 @@ class FnrTest : FunSpec({
     context("gyldig") {
         withData(
             listOf(
-                // Sjekker ikke kontrollsiffer
-                "01010012345",
-                "19092212345",
-                "29104512345",
-                "31129912345",
-                "25056712345",
+                "01010012356",
+                "19092212338",
+                "29104512346",
+                "31129912319",
+                "25056812310",
                 "11085812345",
-                "41066612345", // D-nummer
-                "45066612345", // D-nummer
-                "50066612345", // D-nummer
-                "57066612345", // D-nummer
-                "60066612345", // D-nummer
-                "69066612345", // D-nummer
-                "70066612345", // D-nummer
-                "71066612345", // D-nummer
-                "01490012345", // Testperson fra NAV
-                "01500012345", // Testperson fra NAV
-                "01890012345", // Testperson fra TestNorge
-                "01900012345" // Testperson fra TestNorge
+                "41066712395", // D-nummer
+                "45066612310", // D-nummer
+                "50066612306", // D-nummer
+                "56066612320", // D-nummer
+                "60066612387", // D-nummer
+                "69066612309", // D-nummer
+                "70066712379", // D-nummer
+                "71066612397", // D-nummer
+                "01490012369", // Testperson fra NAV
+                "01500012397", // Testperson fra NAV
+                "01890012341", // Testperson fra TestNorge
+                "01900912338" // Testperson fra TestNorge
             )
         ) {
             shouldNotThrowAny {
@@ -41,24 +40,38 @@ class FnrTest : FunSpec({
     context("ugyldig") {
         withData(
             listOf(
-                "00010012345", // dag 0, andre siffer feil
-                "32010012345", // dag 32, andre siffer feil
-                "40010012345", // dag 40, andre siffer feil (D-nummer)
-                "72010012345", // dag 72, andre siffer feil (D-nummer)
-                "80010012345", // dag 80, første siffer feil
-                "01000012345", // måned 0, fjerde siffer feil
-                "01130012345", // måned 13, fjerde siffer feil
-                "01200012345", // måned 20, tredje siffer feil
-                "01390012345", // måned 39, tredje og fjerde siffer feil
-                "01400012345", // måned 40, fjerde siffer feil (testperson)
-                "01530012345", // måned 53, fjerde siffer feil (testperson)
-                "01790012345", // måned 79, tredje og fjerde siffer feil
-                "01800012345", // måned 80, fjerde siffer feil (testperson)
-                "01930012345", // måned 93, fjerde siffer feil (testperson)
+                "00010012317", // dag 0, andre siffer feil
+                "32010012308", // dag 32, andre siffer feil
+                "40010012300", // dag 40, andre siffer feil (D-nummer)
+                "72010012482", // dag 72, andre siffer feil (D-nummer)
+                "80010012485", // dag 80, første siffer feil
+                "01000012366", // måned 0, fjerde siffer feil
+                "01130012384", // måned 13, fjerde siffer feil
+                "01200012352", // måned 20, tredje siffer feil
+                "01390012310", // måned 39, tredje og fjerde siffer feil
+                "01400012349", // måned 40, fjerde siffer feil (testperson)
+                "01530012367", // måned 53, fjerde siffer feil (testperson)
+                "01790012484", // måned 79, tredje og fjerde siffer feil
+                "01800012321", // måned 80, fjerde siffer feil (testperson)
+                "01930012420", // måned 93, fjerde siffer feil (testperson)
                 "0101001234", // for kort
-                "010100123456", // for langt
+                "010100123567", // for langt
                 "010100x2345", // med bokstav
-                "010100 1234" // med mellomrom
+                "010100 1234", // med mellomrom
+                "01010012313", // ugyldig kontrollsiffer 1
+                "19092212389", // ugyldig kontrollsiffer 1
+                "060606123-3", // ugyldig kontrollsiffer 1 (sjekksum = 10)
+                "060606123103", // ugyldig kontrollsiffer 1 (sjekksum = 10)
+                "41066712336", // ugyldig kontrollsiffer 1 (D-nummer)
+                "01490012326", // ugyldig kontrollsiffer 1 (Testperson fra NAV)
+                "01890012309", // ugyldig kontrollsiffer 1 (Testperson fra TestNorge)
+                "29104512344", // ugyldig kontrollsiffer 2
+                "31129912310", // ugyldig kontrollsiffer 2
+                "1403551234-", // ugyldig kontrollsiffer 2 (sjekksum = 10)
+                "140355123410", // ugyldig kontrollsiffer 2 (sjekksum = 10)
+                "45066612316", // ugyldig kontrollsiffer 2 (D-nummer)
+                "01500012390", // ugyldig kontrollsiffer 2 (Testperson fra NAV)
+                "01900912339" // ugyldig kontrollsiffer 2 (Testperson fra TestNorge)
             )
         ) {
             shouldThrowExactly<IllegalArgumentException> {
@@ -74,7 +87,7 @@ class FnrTest : FunSpec({
     }
 
     test("toString gir wrappet verdi") {
-        Fnr("24120612345").let {
+        Fnr("24120612359").let {
             it.toString() shouldBe it.verdi
         }
     }
--- src/test/kotlin/no/nav/helsearbeidsgiver/utils/wrapper/OrgnrTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/utils/wrapper/OrgnrTest.kt
@@ -11,12 +11,12 @@ class OrgnrTest : FunSpec({
     context("gyldig") {
         withData(
             listOf(
-                "161231654",
-                "908498460",
-                "135103210",
+                "161231657",
+                "908498461",
+                "135103217",
                 "684603132",
-                "167484979",
-                "796033020"
+                "167484972",
+                "796033029"
             )
         ) {
             shouldNotThrowAny {
@@ -28,10 +28,14 @@ class OrgnrTest : FunSpec({
     context("ugyldig") {
         withData(
             listOf(
-                "123", // for kort
-                "0123456789", // for langt
-                "12x456789", // med bokstav
-                "1234 6789" // med mellomrom
+                "1233", // for kort
+                "1234567895", // for langt
+                "12x4567890", // med bokstav
+                "1234 67892", // med mellomrom
+                "161231654", // ugyldig kontrollsiffer
+                "908498464", // ugyldig kontrollsiffer
+                "65432445-", // ugyldig kontrollsiffer (sjekksum = 10)
+                "6543244510" // ugyldig kontrollsiffer (sjekksum = 10)
             )
         ) {
             shouldThrowExactly<IllegalArgumentException> {
@@ -47,7 +51,7 @@ class OrgnrTest : FunSpec({
     }
 
     test("toString gir wrappet verdi") {
-        Orgnr("123456789").let {
+        Orgnr("123456785").let {
             it.toString() shouldBe it.verdi
         }
     }
